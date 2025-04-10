e31155dcd745d3671abddc08c27b7603418c4d9a
EvanZouQ <65980596+EvanZouQ@users.noreply.github.com>
2025-04-10 20:13:12 +0900

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -212,40 +212,40 @@ const srtop = ScrollReveal({
 });
 
 /* SCROLL HOME */
-srtop.reveal('.home .content h3', { delay: 200 });
-srtop.reveal('.home .content p', { delay: 200 });
-srtop.reveal('.home .content .btn', { delay: 200 });
+srtop.reveal('.home .content h3', { delay: 50 });
+srtop.reveal('.home .content p', { delay: 50 });
+srtop.reveal('.home .content .btn', { delay: 50 });
 
-srtop.reveal('.home .image', { delay: 400 });
-srtop.reveal('.home .linkedin', { interval: 600 });
-srtop.reveal('.home .github', { interval: 800 });
-srtop.reveal('.home .twitter', { interval: 1000 });
-srtop.reveal('.home .telegram', { interval: 600 });
-srtop.reveal('.home .instagram', { interval: 600 });
+srtop.reveal('.home .image', { delay: 50 });
+srtop.reveal('.home .linkedin', { interval: 50 });
+srtop.reveal('.home .github', { interval: 50 });
+srtop.reveal('.home .twitter', { interval: 50 });
+srtop.reveal('.home .telegram', { interval: 50 });
+srtop.reveal('.home .instagram', { interval: 50 });
 srtop.reveal('.home .dev', { interval: 600 });
 
 /* SCROLL ABOUT */
-srtop.reveal('.about .content h3', { delay: 200 });
-srtop.reveal('.about .content .tag', { delay: 200 });
-srtop.reveal('.about .content p', { delay: 200 });
-srtop.reveal('.about .content .box-container', { delay: 200 });
-srtop.reveal('.about .content .resumebtn', { delay: 200 });
+srtop.reveal('.about .content h3', { delay: 50 });
+srtop.reveal('.about .content .tag', { delay: 50 });
+srtop.reveal('.about .content p', { delay: 50 });
+srtop.reveal('.about .content .box-container', { delay: 50 });
+srtop.reveal('.about .content .resumebtn', { delay: 50 });
 
 
 /* SCROLL experiences */
-srtop.reveal('.experiences .container', { interval: 200 });
-srtop.reveal('.experiences .container .bar', { delay: 400 });
+srtop.reveal('.experiences .container', { interval: 50 });
+srtop.reveal('.experiences .container .bar', { delay: 50 });
 
-/* SCROLL EDUCATION */
-srtop.reveal('.education .box', { interval: 200 });
+/* SCROLL Projects */
+srtop.reveal('.projects .box', { interval: 50 });
 
-/* SCROLL PROJECTS */
-srtop.reveal('.work .box', { interval: 200 });
+/* SCROLL Publications */
+srtop.reveal('.publications .box', { interval: 50 });
 
 /* SCROLL EXPERIENCE */
-srtop.reveal('.experience .timeline', { delay: 400 });
-srtop.reveal('.experience .timeline .container', { interval: 400 });
+srtop.reveal('.achievements .timeline', { delay: 50 });
+srtop.reveal('.achievements .timeline .container', { interval: 50 });
 
 /* SCROLL CONTACT */
-srtop.reveal('.contact .container', { delay: 400 });
-srtop.reveal('.contact .container .form-group', { delay: 400 });
+srtop.reveal('.contact .container', { delay: 50 });
+srtop.reveal('.contact .container .form-group', { delay: 50 });
